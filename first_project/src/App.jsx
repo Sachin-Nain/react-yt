45ca96afd928cc147fe2dd844ccfdb5c13ee62dc
sachin <sachinlitani@gmail.com>
2026-01-29 12:19:30 +0530

    uploading files to github

---
+++ first_project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to My First React Project!</h1>
+      <p>This is a simple React application.</p>
+    </>
+  );
+};
+
+export default App;
